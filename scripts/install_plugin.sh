a15a3ae5626c9eabc5f59c8e32aaad8abfe43bdb
Akshay Jagadish <akshaysajnani1996@gmail.com>
2021-11-16 17:18:34 -0500

    Update install_plugin.sh

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -25,11 +25,9 @@ mkdir -p "bin"
 mkdir -p "releases/v${version}"
 
 # Download with curl if possible.
-if [ -x "$(which curl 2>/dev/null)" ]; then
+
     curl -sSL "${url}" -o "releases/v${version}.tar.gz"
-else
-    wget -q "${url}" -O "releases/v${version}.tar.gz"
-fi
+
 tar xzf "releases/v${version}.tar.gz" -C "releases/v${version}"
 mv "releases/v${version}/bin/helm-cm-push" "bin/helm-cm-push" || \
     mv "releases/v${version}/bin/helm-cm-push.exe" "bin/helm-cm-push"
